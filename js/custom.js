3cdaf74c0f7ae4fdc54390fed59a4b99f6f73529
jiyoungbb <jiiyoung0906@naver.com>
2022-06-10 18:43:11 +0900

    home section done

--- js/custom.js
+++ js/custom.js
@@ -7,4 +7,24 @@ $(document).ready(function(){
     $('section').click(function(){
         $('.menu_btn, .menu_btn span, .nav').removeClass('active')
     })
+
+    /* */
+    $(window).scroll(function(){
+        if($(window).scrollTop() == 0) {
+          $('.loading').removeClass('active')
+        }
+        else {
+          $('.loading').addClass('active')
+        }   
+      })
+
+      /* introduction */
+      $(window).scroll(function(){
+        if($(window).scrollTop() > 410) {
+          $('.intro').addClass('active')
+        }
+        else {
+          $('.intro').removeClass('active')
+        }   
+      })
 })
