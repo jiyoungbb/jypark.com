03717aea458a75f0915f801bd08b644fd7597804
jiyoungbb <jiiyoung0906@naver.com>
2022-07-01 15:08:40 +0900

    smooth scrolling

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 
+    /* smooth scrolling */
+    $('#home_btn, #about_btn, #skills_btn, #projects_btn, #pr_btn, #contact_btn').click(function () {
+      $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+      }, 500);
+      return false;
+    });
+
+
     /* nav */
     $('.menu_btn').click(function(){
         $('.menu_btn, .menu_btn span, .nav').toggleClass('active')
